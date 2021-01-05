a92993a6111e2341beb569a24032172a65ce8d4c
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2021-01-04 18:25:01 -0800

    user found

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 'use strict'
 
 var button = document.querySelector("#button");
+var div_user = document.querySelector("#user")
 
 button.addEventListener("click", () => {
-    console.log("test passed");
+    
+let id = document.querySelector("#userName").value;
+    fetch("https://reqres.in/api/users/"+id)
+        .then(data => data.json())
+        .then(user => {
+            console.log(user.data);
+            let nombre = document.createElement("h2");
+            nombre.innerHTML = user.first_name+" "+user.last_name;
+            div_user.appendChild(nombre);
+            console.log(user.first_name+" "+user.last_name);
+
+        })
 });
+        
